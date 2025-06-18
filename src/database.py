6a21e54e49492f3515c1da04c40e06e845a39d09
Dienly-Bogdan <bkulinichlol@yandex.ru>
2025-06-18 13:35:57 +0300

    Добавление метода close_db

--- src/database.py
+++ src/database.py
@@ -6,9 +6,16 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DB_PATH = os.path.join(BASE_DIR, "pasta_pizza.db")
 
 
+
 def get_db():
     if 'db' not in g:
         conn = sqlite3.connect(DB_PATH)
         conn.row_factory = sqlite3.Row
         g.db = conn
     return g.db
+
+
+def close_db(e=None):
+    db = g.pop('db', None)
+    if db is not None:
+        db.close()
