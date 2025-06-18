c4fe01531cac26e12d6e26f975b235f6d5d273e0
Dienly-Bogdan <bkulinichlol@yandex.ru>
2025-06-18 13:35:32 +0300

    Добавление метода get_db

--- src/database.py
+++ src/database.py
@@ -4,3 +4,11 @@ from flask import g
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DB_PATH = os.path.join(BASE_DIR, "pasta_pizza.db")
+
+
+def get_db():
+    if 'db' not in g:
+        conn = sqlite3.connect(DB_PATH)
+        conn.row_factory = sqlite3.Row
+        g.db = conn
+    return g.db
