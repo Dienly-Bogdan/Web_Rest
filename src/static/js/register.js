c9d6354e09c7517f838c62eb3ccbc97ecc3110ba
Dienly-Bogdan <bkulinichlol@yandex.ru>
2025-06-18 13:46:39 +0300

    Комментирование кода

--- src/static/js/register.js
+++ src/static/js/register.js
@@ -1,9 +1,14 @@
+// Берём кнопки "Sign Up" и "Sign In" из DOM
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
+
+// Если нажали "Sign Up" — добавляем класс, который включает правую панель (например, форму регистрации)
 signUpButton.addEventListener('click', () => {
     container.classList.add("right-panel-active");
 });
+
+// Если нажали "Sign In" — убираем этот класс, возвращая форму входа
 signInButton.addEventListener('click', () => {
     container.classList.remove("right-panel-active");
 });
